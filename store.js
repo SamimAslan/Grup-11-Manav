9be146f234bea7def3d53378a5ba01ad3ef68a07
Halil-Kuray <halibram33@gmail.com>
2022-02-12 21:59:52 +0100

    bilgisi alinan urunler listeye yazildi

--- store.js
+++ store.js
@@ -10,5 +10,38 @@ function addToClickBox (event) {
     let theTitle = shopItem.getElementsByClassName("shop-item-title")[0].innerText
     let imageSource = shopItem.getElementsByClassName("shop-item-image")[0].src
     let itemPrice = shopItem.getElementsByClassName("shop-item-price")[0].innerText
-    console.log(theTitle,imageSource, itemPrice)
+    addShoppingBox(theTitle,imageSource, itemPrice)
 } 
+function  addShoppingBox(theTitle, imageSource, itemPrice) {
+    let container = document.createElement('div');
+    container.classList.add('cart-row');
+
+
+    let cardItemsContainer = document.getElementsByClassName('cart-items')[0];
+    let boxItemsTitlesList = cardItemsContainer.querySelector('.cart-item-title');
+
+//    for (let index = 0; index < boxItemsTitlesList.length; index++) {
+       
+//         if (boxItemsTitlesList[index].innerText == theTitle) {
+//             alert('bu urun zaten sepette var');
+//             return 
+//         }
+       
+//    }
+
+    container.innerHTML +=`
+
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${imageSource}" width="100" height="100">
+        <span class="cart-item-title">${theTitle}</span>
+    </div>
+
+    <span class="cart-price cart-column">${itemPrice}</span>
+
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button">REMOVE</button>
+    </div>
+    `
+    cardItemsContainer.append(container);
+}
